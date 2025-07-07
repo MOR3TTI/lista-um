d647bcdc1aac084f57b7342bcdfe9b499ec2bb28
MOR3TTI <vinicius_b_moretti@estudante.sesisenai.org.br>
2025-07-07 09:33:14 -0300

    update - um

---
+++ exencicio01.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Verificar Par ou Ímpar</title>
+</head>
+<body>
+
+<form method="POST">
+    <label>Digite um número:</label>
+    <input type="number" name="numero" required>
+    <button type="submit">Verificar</button>
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $numero = $_POST["numero"];
+
+    if ($numero % 2 == 0) {
+        echo "<p>O número <strong>$numero</strong> é <strong>PAR</strong>.</p>";
+    } else {
+        echo "<p>O número <strong>$numero</strong> é <strong>ÍMPAR</strong>.</p>";
+    };
+};
+?>
+
+</body>
+</html>
